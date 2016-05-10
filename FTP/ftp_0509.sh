27ba81ec596bc34c8da478ab600e1f5981d270b0
Ilikeperl <yiwei.shih@gmail.com>
2016-05-09 17:59:25 -0700

    Move to real Server
    Why I cannot assign sender !!?
    
    mail "test" | mailx -r IT@sparqtron.com -s "Test" aaa@sparqtron.com ( It
    not work )
    mail "test" | mailx -r IT@sparqtron.com -s "Test" aaa@gamil.com ( It not
    work )

---
+++ FTP/ftp_0509.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Program:
+#       Detective FTP server folder has whether new file  and inform user
+# History:
+# 2016/05/05    Yiwei Shih      First release
+
+
+path="/home/sparq02/"
+
+mail=($(cat /home/sparq02/mail.txt))   #store cat contents in array
+
+folder="`date +%b`_`date +%Y`"
+
+arr=($( find $path -maxdepth 1 -name "*.pdf"))
+
+
+if [ ${#arr[@]} != 0 ]; then                                    #if array is not equal null
+        for ((i=0; i<${#arr[@]}; i++)); do                      #"${#arr[@]}" means length of array
+                if [ -d $path$folder ]; then
+                        mv ${arr[$i]} $path$folder
+                else
+                        mkdir $path$folder && mv ${arr[$i]} $path$folder
+                fi
+        done
+        for ((k=0; k<${#mail[@]}; k++)); do
+                if [ "${#arr[@]}" > 1 ]; then
+                        echo "Today, we got ${#arr[@]} packing slips" | mail -s "File update inform" ${mail[$k]}
+                else
+                        echo "Today, we got ${#arr[@]} packing slip" | mail -s "File update inform" ${mail[$k]}
+                fi
+         done
+exit
+fi
+
+#If I assign internal email address to send mail, the other internal email will not recevie mail, external mail is OK ! Why?
+#For example:
+#echo "Test mail" | mail -r IT@sparqtron.com -s "This is test mail" aaa@gmail.com  ===> It worked
+#echo "Test mail" | mail -r IT@sparqtron.com -s "This is test mail" bbb@sparqtron.com  ===> It not worked
